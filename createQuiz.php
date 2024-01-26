fcfcea4ec0646abf38262c50a88243577a6a8827
laighton <bacalso.ton2x@gmail.com>
2024-01-26 16:54:22 +0800

    added createQuiz page
    also -added login button in navbar

---
+++ createQuiz.php
@@ -0,0 +1,16 @@
+<!-- header -->
+<?php require('assets/php/head.inc.php'); ?>
+
+<!-- navbar -->
+<?php include('assets/php/navbar.inc.php'); ?>
+
+<div class="container">
+    <div class="container m-5"></div>
+    <div>
+        <p class="bg-light">hello</p>
+    </div>
+</div>
+
+
+
+<?php require('assets/php/footer.inc.php'); ?>
