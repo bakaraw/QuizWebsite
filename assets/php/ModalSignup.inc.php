437b703b5fcf3ffccaa4b5028cf65d5c0d9e2fb3
leanzack <138343724+leanzack@users.noreply.github.com>
2024-02-18 08:18:46 +0800

    Account is done najud
    required to login to click any shit here change modal pero wala pa desgin

--- assets/php/ModalSignup.inc.php
+++ assets/php/ModalSignup.inc.php
@@ -1,29 +1,28 @@
-<div class="modal fade" id="modalSignup" tabindex="-1" role="dialog" aria-labelledby="SignupModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header text-center">
-                    <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
-                    <a href="index.php" class="close" aria-label="Close" style="text-decoration: none; display: inline-block; color: inherit; background-color: transparent; border: none; cursor: pointer;">
-                    <span aria-hidden="true">&times;</span>
-                  </a>
-                </div>
-                <div class="modal-body mx-3">
-                    <div class="md-form mb-5">
-                        <i class="fas fa-user prefix grey-text"></i>
-                        <input type="text" id="username" name="username" class="form-control validate">
-                        <label data-error="wrong" data-success="right" for="username">Username</label>
+<!-- Enhanced Modern Signup Modal -->
+<div class="modal fade" id="modalSignup" tabindex="-1" aria-labelledby="SignupModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title text-center w-100" id="SignupModalLabel ">Create Account</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body p-4">
+                <form>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
                     </div>
-
-                    <div class="md-form mb-4">
-                        <i class="fas fa-lock prefix grey-text"></i>
-                        <input type="password" id="pass" name="pass" class="form-control validate">
-                        <label data-error="wrong" data-success="right" for="pass">Password</label>
+                    
+                    <div class="mb-4">
+                        <input type="password" class="form-control" id="pass" name="pass" placeholder="Password" required>
                     </div>
-
-                    <div class="modal-footer d-flex justify-content-center">
-                        <button type="submit" name="submit" class="btn btn-default">Sign up</button>
+                    <div class="text-center">
+                        <button type="submit" name="submit" class="btn btn-primary w-100">Sign Up</button>
                     </div>
-                </div>
+                </form>
+            </div>
+            <div class="modal-footer justify-content-center">
+                <p class="text-muted">Already have an account? <a href="#" class="text-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">Log in</a></p>
             </div>
         </div>
     </div>
+</div>
