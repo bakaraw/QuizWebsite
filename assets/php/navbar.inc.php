dc36f460f39a1e1178cb70687374db6a6208d115
laighton <bacalso.ton2x@gmail.com>
2024-02-19 18:25:40 +0800

    ernest changes

--- assets/php/navbar.inc.php
+++ assets/php/navbar.inc.php
@@ -26,7 +26,7 @@ if (isset($_GET['logout']) && $_GET['logout'] == '1') {
         <li class="nav-item">
         <?php if (isset($_SESSION['username'])): ?>
 
-          <a class="nav-link" href="MakeQuiz.php">Quiz List</a>
+          <a class="nav-link" href="MakeQuiz.php">Create quiz</a>
           <?php else: ?>
           <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#modalLogin">Quiz List</a>
         <?php endif; ?>
@@ -36,7 +36,7 @@ if (isset($_GET['logout']) && $_GET['logout'] == '1') {
 
           <a class="nav-link" href="index.php">Create Quiz</a>
           <?php else: ?>
-          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#modalLogin">Create Quiz</a>
+          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#modalLogin">Quiz List</a>
         <?php endif; ?>
         </li>
       </ul>
